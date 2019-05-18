984ce70f0c4c6f1fe3749502bac5f5912212dc75
QDMQ <1157762555@qq.com>
2019-05-18 15:10:12 +0800

    add newQuestion

--- pages/newQuestion/newQuestion.js
+++ pages/newQuestion/newQuestion.js
@@ -5,7 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+      operation: 'check',
+      content: '1：我想要加盟一下云南十里香小锅米线，但是有什么注意s事项，如果来开始，请有经验的前辈指导一下，网上搜的勿扰~'
+  },
+
+  switchChange(e) {
+    console.log('switch1 发生 change 事件，携带值为', e.detail.value)
   },
 
   /**
